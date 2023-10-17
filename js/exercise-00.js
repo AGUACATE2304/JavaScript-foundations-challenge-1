6ec9c6f92ad95b89d82be654a193f3549403307c
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2023-10-17 20:30:02 +0200

    Complete exercice-00 and Index.html updated

---
+++ js/exercise-00.js
@@ -0,0 +1,22 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+
+const stringValEx00 = "Exercice-00";
+const numberValEx00 = 0;
+const booleanValEx00 = true;
+const nullValEx00 = null;
+const undefinedValEx00 = undefined;
+
+console.log(stringValEx00);
+console.log(numberValEx00);
+console.log(booleanValEx00);
+console.log(nullValEx00);
+console.log(undefinedValEx00);
