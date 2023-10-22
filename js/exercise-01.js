210a212449091d47e892f1cdc64851c4222c1ee2
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2023-10-22 22:02:47 +0200

    completed exercice-01.js

---
+++ js/exercise-01.js
@@ -0,0 +1,26 @@
+/* 
+    1. Create a variable called 'name' that points at a string containing your name
+    2. Define function 'updateValue', which receives a value and updates variable 'name'
+    3. On the next line, call function 'updateValue' with your favourite musician's name
+      Log 'name' value.
+    4. On the next line, call function 'updateValue' with value 'true'
+      Log 'name' value.
+    5. On the next line, call function 'updateValue' value '28'
+      Log 'name' value.
+
+*/
+
+const name = "Alan Castillo";
+let userName = name;
+
+function updateValue(newName){
+  userName = newName;
+  return userName;
+}
+
+updateVaule("Hard GZ");
+console.log(name);
+updateValue(true);
+console.log(name);
+updateValue('28');
+console.log(name);
