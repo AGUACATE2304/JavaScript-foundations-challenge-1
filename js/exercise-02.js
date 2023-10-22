74c831cfa7be7f69ab14d14d8fddbf5bcd556327
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2023-10-22 22:10:43 +0200

    completed exercice-02.js

---
+++ js/exercise-02.js
@@ -0,0 +1,25 @@
+/* 
+    1. Declare a variable using let with any name you like, but without a value.
+    2. On a separate line, assign the variable the String: "Moo!"
+    3. Log out this variable to the terminal using console.log
+    
+    Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
+*/
+
+let letVariable;
+letVariable = "Moo!";
+console.log(letVariable);
+
+
+// I comented the code, because it appears and error
+
+/*
+const constVariable;
+constVariable ="Moo!";
+conosle.log(constVariable);
+
+A Syntax Error apears, because const is a type of variable that need to be initialized at first,
+and can not be changed or reasigned.
+This means that you can not declare a const and in an other line change his value;
+*/
+
